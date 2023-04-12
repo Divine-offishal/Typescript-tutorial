040b224e0e9cb0999a821601534675fa7f3fc74b
Divine Udise <divineoffishal@gmail.com>
2023-04-12 10:04:04 +0100

    🔥 Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TYPESCRIPT
+
+Install typescript compiler globally and then to watch file changes, tsc filename.ts -w
--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,8 @@
-var character = 'mario';
-console.log(character);
-var inputs = document.querySelectorAll('input');
-console.log(inputs);
-inputs.forEach(function (input) {
-    console.log(input);
-});
+var character = 'luigi';
+var age = 30;
+var isBlackbelt = false;
+character = 'mario';
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(7.5));
